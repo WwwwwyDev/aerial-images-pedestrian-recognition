891268fe56df435c535cd30ade1955c5f950e804
WwyDev <wwwwwydev@gmail.com>
2025-12-27 17:29:13 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo12s.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo12m.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
 results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
